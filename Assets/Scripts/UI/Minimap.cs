435b6f5b49934c5ff926586b7ff20ecda38f1437
Al-Goodman <121xXAlvitosXx121@gmail.com>
2026-02-26 15:49:29 -0800

    MINIMAP
    Brought back the minimap, hopefully for good this time!

--- Assets/Scripts/UI/Minimap.cs
+++ Assets/Scripts/UI/Minimap.cs
@@ -30,6 +30,7 @@ public class Minimap : MonoBehaviour
         transform.position = newPosition;
 
         // this line lets the minimap rotate based on if the player turns
-        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+        // commented it out, I like the way the minimap moves when it's not turning with player movement, if you wanna keep it, just uncomment the line -Salvador
+        //transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
     }
 }
