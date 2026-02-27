09e192be595ce8ffb15a9c090454d10c1d0929e8
EthanBCal <Ethanbryantcal@gmail.com>
2026-02-27 11:23:08 -0800

    added the sell desk to the shop scene and edited the ShopPodium.cs script to allow selling plants based off plant inventory count which is gathered from PlantInventory.cs which should be attached to player. PlantCount is increased from FarmTile.cs

--- Assets/Scripts/Farming/PlantInventory.cs
+++ Assets/Scripts/Farming/PlantInventory.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+<<<<<<< HEAD
 using System;
 
 namespace Farming
@@ -72,3 +73,27 @@ namespace Farming
         }
     }
 }
+=======
+
+namespace Farming
+{
+    public class PlantInventory : MonoBehaviour
+{
+    [SerializeField] private int plants = 0;
+
+    public int PlantCount => plants;   // ✅ FIX
+
+    public void AddPlant(int amount = 1)
+    {
+        plants += Mathf.Max(1, amount);
+    }
+
+    public bool TryRemovePlants(int amount)
+    {
+        if (plants < amount) return false;
+        plants -= amount;
+        return true;
+    }
+}
+}
+>>>>>>> d01161f (added the sell desk to the shop scene and edited the ShopPodium.cs script to allow selling plants based off plant inventory count which is gathered from PlantInventory.cs which should be attached to player. PlantCount is increased from FarmTile.cs)
